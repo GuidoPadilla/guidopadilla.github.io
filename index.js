c85322c2b02586a9966baedb1057471d54c0b180
GuidoPadilla <46847917+GuidoPadilla@users.noreply.github.com>
2022-09-06 19:31:18 -0600

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable react/jsx-filename-extension */
-import ReactDom from 'react-dom'
-import React from 'react'
-import App from './components/App'
-
-ReactDom.render(
-  <App />,
-  document.getElementById('root'),
-)
